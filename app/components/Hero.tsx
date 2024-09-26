2d69dac527f826837ca8a3b8054289350759f7f9
DarcXnite <anhri89@yahoo.com>
2024-09-26 01:11:55 -0500

    added more UI updates

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,6 +3,7 @@ import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 import LitUpBtn from './ui/LitUpBtn'
 import { FaLocationArrow } from 'react-icons/fa6'
+import { Cover } from './ui/Cover'
 
 function Hero() {
   return (
@@ -19,10 +20,9 @@ function Hero() {
         <Spotlight className='top-28 left-80 h-[80vh] w-[50vh]' fill='blue' />
       </div>
 
-      <div className='h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0'>
-        {/* Radial gradient for the container to give a faded look */}
+      {/* <div className='h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0'>
         <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]' />
-      </div>
+      </div> */}
 
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
@@ -34,9 +34,12 @@ function Hero() {
             className='text-center text-[40px] md:text-5xl lg:text-6xl'
             words='Hi, My Name is Davis Nguyen'
           />
-          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-            I&apos;m developer based in Chicago
-          </p>
+          <div className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+            I&apos;m a developer based in{' '}
+            <Cover>
+              <span className='text-cyan-400'>Chicago</span>
+            </Cover>
+          </div>
           <a href='#about'>
             <LitUpBtn
               title='Projects'
